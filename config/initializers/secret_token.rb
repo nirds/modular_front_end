43768f27a1f9d6a60c9e50770e51f26c0ee7319c
Adam Hendricksen <adam@nird.us>
2013-09-03 12:32:37 -0700

    Adding new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ModularFrontEnd::Application.config.secret_key_base = '9fa138cf4a03dd133a5018ee2dfaaa67766cb29743b0ee793095d101c3c422ee1112def4112ce8c1be7c969f127e64b727b9f40073df35ffacb96874d4af910c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ModularFrontEnd::Application.config.session_store :cookie_store, key: '_modular_front_end_session'
